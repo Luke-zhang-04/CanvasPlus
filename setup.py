dd55e9d1017211aa77976e11a1a0281d03969eff
Luke <55749227+Luke-zhang-04@users.noreply.github.com>
2020-03-30 11:23:42 -0400

    modified usernaem

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="CanvasPlus-LUKE-ZHANG-04",
+    name="CanvasPlus-Luke-zhang-04",
     version="0.0.1",
     author="Luke Zhang",
     author_email="luke.zhang2004@gmail.com",
