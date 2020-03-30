7d06e47c0f9a8e3329ceaba8da6b2132832960ca
Luke <55749227+Luke-zhang-04@users.noreply.github.com>
2020-03-30 10:28:11 -0400

    more direct function calls, exceptions for rotate

--- CanvasPlus.py
+++ CanvasPlus.py
@@ -1,15 +1,29 @@
+#tkinter
 from tkinter import Canvas
+
+#complex numbers and stuff
+import cmath, math
+
+#stuff for typing hints
+from numbers import Real
+
+class Error(Exception):
+   '''Base class for other exceptions'''
+   pass
+
+class InvalidUnitError(Error):
+    '''Raised when unit is not recognised'''
+    pass
 
 class CanvasPlus(Canvas):
     '''Improved Canvas widget with more functionality to display graphical elements like lines or text.'''
 
-    def create_circle(self, x, y, radius, **kwargs):
+    def create_circle(self, x: Real, y: Real, radius: Real, **kwargs) -> int:
         '''Create circle with coordinates x, y, radius'''
-        return self.create_oval(x+radius, y+radius, x-radius, y-radius, **kwargs)
+        return self._create('oval', [x+radius, y+radius, x-radius, y-radius], kwargs)
 
-    def create_round_rectangle(self, x1, y1, x2, y2, radius=25, **kwargs):
-        '''Create circle with coordinates x1, y1, x2, y2, radius = var (default 25)'''
-
+    def create_round_rectangle(self, x1: Real, y1: Real, x2: Real, y2: Real, radius: Real = 25, **kwargs) -> int:
+        '''Create circle with coordinates x1, y1, x2, y2, radius = val (default 25)'''
         points = [
             x1+radius, y1,
             x1+radius, y1,
@@ -33,8 +47,34 @@ class CanvasPlus(Canvas):
             x1, y1
         ]
 
-        return self.create_polygon(*points, smooth=True, **kwargs)
+        kwargs["smooth"] = True
+        return self._create('polygon', points, kwargs)
 
+    def create_arrow(self, x1: Real, y1: Real, headLength: Real, headWidth: Real, bodyLength: Real, bodyWidth: Real, **kwargs) -> int:
+        '''Create arrow with x1, y1 as the tip; headWith, headLengh as the length and width of the arrowhead; and bodyLength, bodyWidth as the length and width of the arrow body, as well as direction = val (0 by default)'''
+        
+        points = [
+            x1, y1,
+            x1-headWidth//2, y1+headLength,
+            x1-bodyWidth//2, y1+headLength,
+            x1-bodyWidth//2, y1+bodyLength,
+            x1+bodyWidth//2, y1+bodyLength,
+            x1+bodyWidth//2, y1+headLength,
+            x1+headWidth//2, y1+headLength
+        ]
+        
+        return self._create('polygon', points, kwargs)
+
+    def rotate(self, obj: int, x: Real, y: Real, amount: Real, unit: str = "rad") -> None:
+        '''rotate obj on axis x, y by amount in degrees or radians clockwise'''
+        if unit == "d" or unit == "deg" or unit == "degree" or unit == "degrees":
+            radians = False
+        elif unit == "r" or unit == "rad" or unit == "radian" or unit == "radians":
+            radians = True
+        else:
+            raise InvalidUnitError("Invalid unit \"" + unit + "\"")
+
+        
 
 def _test():
     from tkinter import Tk
@@ -43,7 +83,9 @@ def _test():
     canvas.pack()
 
     canvas.create_circle(300, 300, 100, fill = "black", outline = "green", width = 3)
-    canvas.create_round_rectangle(400, 400, 500, 500, radius = 50, fill = "blue", outline = "orange", width = 5)
+    canvas.create_round_rectangle(400, 400, 500, 500, radius = 75, fill = "blue", outline = "orange", width = 5)
+    canvas.create_arrow(600, 600, 50, 50, 150, 20, fill = "grey", outline = "black")
+    canvas.rotate(1, 1, 1, 1)
 
     canvas.update()
     canvas.mainloop()
--- readme.md
+++ readme.md
@@ -9,7 +9,13 @@ Canvas.create_circle(x, y, radius, **kwargs)
 Creates a circle with the centre x, y, and a radius, rather than create_oval(x1, y1, x2, y2, **kwargs), which can be unintuitive to use in many situations.
 
 ## create_round_rectangle ##
-Canvas.create_round_rectangle(x1, y1, x2, y2, radius = var, **kwargs)
+Canvas.create_round_rectangle(x1, y1, x2, y2, radius = val, **kwargs)
 
 radius = 25 by default<br>
 Creates a rectangle with rounded corners based on radius. Rounded rectangles are hard to make and are useful in many situations.
+
+## create_arrow ##
+Canvas.create_arrow(x1, y1, headLength, headWidth, bodyLength, bodyWidth, direction = val, **kwargs)
+
+direction = 0 by default (degrees)<br>
+Create arrow with x1, y1 as the tip; headWith, headLengh as the length and width of the arrowhead; and bodyLength, bodyWidth as the length and width of the arrow body.
