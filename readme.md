c786c12a7a2c8f2ccd07d4e2d6e620eac400d728
Luke <55749227+Luke-zhang-04@users.noreply.github.com>
2020-03-30 11:22:25 -0400

    added setup.py

--- readme.md
+++ readme.md
@@ -1,5 +1,7 @@
 # Canvas Plus #
-Tkinter's Canvas widget has some limitations that I want to try to fix/change with this repository.
+An improved Canvas widget for tkinter with more functionality to display graphical elements like lines or text. 
+
+Tkinter's Canvas widget has some limitations which are adressed in this package.
 
 Simmilar to the default tkinter Canvas widget, e.g canvas.create_rectangle, other canvas objects can be created. 
 
---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="CanvasPlus-LUKE-ZHANG-04",
+    version="0.0.1",
+    author="Luke Zhang",
+    author_email="luke.zhang2004@gmail.com",
+    description="Improved Canvas widget for tkinter with more functionality to display graphical elements like lines or text. ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Luke-zhang-04/CanvasPlus",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.8.0",
+        "Programming Language :: Tcl",
+        "Topic :: Multimedia :: Graphics",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.3',
+)
