3eb45075a39862f39c720c2690f12763b74c5bd0
Luke <55749227+Luke-zhang-04@users.noreply.github.com>
2020-04-12 21:57:47 -0400

    v1.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="CanvasPlus",
-    version="1.1.3",
+    version="1.2.0",
     author="Luke Zhang",
     author_email="luke.zhang2004@gmail.com",
     description="Improved Canvas widget for tkinter with more functionality to display graphical elements like lines or text. ",
@@ -24,7 +24,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Tcl",
         "Topic :: Multimedia :: Graphics",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.3',
