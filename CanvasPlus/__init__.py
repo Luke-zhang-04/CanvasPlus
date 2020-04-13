f4555bd1263dc68e40f43de5bc4af54b1817a99c
Luke <55749227+Luke-zhang-04@users.noreply.github.com>
2020-04-13 12:34:00 -0400

    added python 3.4 and below compatibility

--- CanvasPlus/__init__.py
+++ CanvasPlus/__init__.py
@@ -2,5 +2,5 @@ import sys
     
 try: from CanvasPlus.canvasplus import CanvasPlus
 except:
-    print("Your python verision %s is not compatible with canvasplus. For compatibility, another file is being imported." % sys.version)
+    print("Your python version %s is not compatible with canvasplus. For compatibility, another file is being imported." % sys.version)
     from CanvasPlus.pythonBelow35 import CanvasPlus
