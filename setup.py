d35b0c15e8020e356f75fc7ed8dd0d3ab901a970
Luke <55749227+Luke-zhang-04@users.noreply.github.com>
2020-04-12 22:09:00 -0400

    spaced code

--- setup.py
+++ setup.py
@@ -4,16 +4,17 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="CanvasPlus",
-    version="1.2.0",
-    author="Luke Zhang",
-    author_email="luke.zhang2004@gmail.com",
-    description="Improved Canvas widget for tkinter with more functionality to display graphical elements like lines or text. ",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/Luke-zhang-04/CanvasPlus",
-    packages=setuptools.find_packages(),
-    classifiers=[
+    name = "CanvasPlus",
+    version = "1.2.0",
+    author = "Luke Zhang",
+    author_email = "luke.zhang2004@gmail.com",
+    description = "Improved Canvas widget for tkinter with more functionality to display graphical elements like lines or text. ",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/Luke-zhang-04/CanvasPlus",
+    packages = setuptools.find_packages(),
+    install_requires = [],
+    classifiers = [
         'Development Status :: 5 - Production/Stable', # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
@@ -27,5 +28,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.3',
+    python_requires = '>=3.3',
 )
